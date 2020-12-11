76386e93397260a8328829f3b993c5b952d82a32
Pedro <pvdo-carmo-saraiva-t@mysenenca.ca>
2020-12-11 16:33:25 -0500

    server2

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+//Install express server
 const express = require('express');
-
+const path = require('path');
+ 
 const app = express();
-
-app.use(express.static('./dist/web422-a4'));
-
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './dist/web422-a4'}
-    );
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/web422-a5'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/web422-a5/index.html'));
 });
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
+ 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
